de9539a4c5381e5084efc473c2efab559536a78c
Cedric Kemp <cedric.kemp@icloud.com>
2017-07-30 16:09:24 -0400

    Release 0.2.0

--- dist/index.js
+++ dist/index.js
@@ -33,32 +33,136 @@ var PARSE_TIMEZONE = 'UTC';
 var cronJobs = {};
 
 /**
- * Stop all scheduled jobs, fetch all jobs from Parse server and schedule crons
+ * Parse job schedule object
+ * @typedef {Object} _JobSchedule
+ * @property {String} id The job id
+ */
+
+/**
+ * Recreate the cron schedules for a specific _JobSchedule or all _JobSchedule objects
+ * @param {_JobSchedule | string} [job=null] The job schedule to recreate. If not specified, all jobs schedules will be recreated.
+ * Can be a _JobSchedule object or the id of a _JobSchedule object.
  */
 var recreateSchedule = function () {
-  var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
-    var results, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, job;
-
+  var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(job) {
+    var jobObject;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
+            if (!job) {
+              _context.next = 25;
+              break;
+            }
+
+            if (!(job instanceof String || typeof job === 'string')) {
+              _context.next = 18;
+              break;
+            }
+
+            _context.prev = 2;
+            _context.next = 5;
+            return _node2.default.Object.extend('_JobSchedule').createWithoutData(job).fetch({
+              useMasterKey: true
+            });
+
+          case 5:
+            jobObject = _context.sent;
+
+            if (!jobObject) {
+              _context.next = 10;
+              break;
+            }
+
+            recreateJobSchedule(jobObject);
+            _context.next = 11;
+            break;
+
+          case 10:
+            throw new Error('No _JobSchedule was found with id ' + job);
+
+          case 11:
+            _context.next = 16;
+            break;
+
+          case 13:
+            _context.prev = 13;
+            _context.t0 = _context['catch'](2);
+            throw _context.t0;
+
+          case 16:
+            _context.next = 23;
+            break;
+
+          case 18:
+            if (!(job instanceof _node2.default.Object && job.className === '_JobSchedule')) {
+              _context.next = 22;
+              break;
+            }
+
+            recreateJobSchedule(job);
+            _context.next = 23;
+            break;
+
+          case 22:
+            throw new Error('Invalid job type. Must be a string or a _JobSchedule');
+
+          case 23:
+            _context.next = 32;
+            break;
+
+          case 25:
+            _context.prev = 25;
+
+            recreateScheduleForAllJobs();
+            _context.next = 32;
+            break;
+
+          case 29:
+            _context.prev = 29;
+            _context.t1 = _context['catch'](25);
+            throw _context.t1;
+
+          case 32:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined, [[2, 13], [25, 29]]);
+  }));
+
+  return function recreateSchedule(_x) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+/**
+ * (Re)creates all schedules (crons) for all _JobSchedule from the Parse server
+ */
+var recreateScheduleForAllJobs = function () {
+  var _ref2 = _asyncToGenerator(regeneratorRuntime.mark(function _callee2() {
+    var results, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, job;
+
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
             if (_node2.default.applicationId) {
-              _context.next = 2;
+              _context2.next = 2;
               break;
             }
 
             throw new Error('Parse is not initialized');
 
           case 2:
-            _context.prev = 2;
-            _context.next = 5;
+            _context2.prev = 2;
+            _context2.next = 5;
             return new _node2.default.Query('_JobSchedule').find({
               useMasterKey: true
             });
 
           case 5:
-            results = _context.sent;
+            results = _context2.sent;
 
 
             destroyJobs();
@@ -66,72 +170,80 @@ var recreateSchedule = function () {
             _iteratorNormalCompletion = true;
             _didIteratorError = false;
             _iteratorError = undefined;
-            _context.prev = 10;
+            _context2.prev = 10;
             for (_iterator = results[Symbol.iterator](); !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
               job = _step.value;
 
               try {
-                cronJobs[job.id] = createCronJobs(job);
+                recreateJobSchedule(job);
               } catch (error) {
                 console.log(error);
               }
             }
-            _context.next = 18;
+            _context2.next = 18;
             break;
 
           case 14:
-            _context.prev = 14;
-            _context.t0 = _context['catch'](10);
+            _context2.prev = 14;
+            _context2.t0 = _context2['catch'](10);
             _didIteratorError = true;
-            _iteratorError = _context.t0;
+            _iteratorError = _context2.t0;
 
           case 18:
-            _context.prev = 18;
-            _context.prev = 19;
+            _context2.prev = 18;
+            _context2.prev = 19;
 
             if (!_iteratorNormalCompletion && _iterator.return) {
               _iterator.return();
             }
 
           case 21:
-            _context.prev = 21;
+            _context2.prev = 21;
 
             if (!_didIteratorError) {
-              _context.next = 24;
+              _context2.next = 24;
               break;
             }
 
             throw _iteratorError;
 
           case 24:
-            return _context.finish(21);
+            return _context2.finish(21);
 
           case 25:
-            return _context.finish(18);
+            return _context2.finish(18);
 
           case 26:
             console.log(Object.keys(cronJobs).length + ' job(s) scheduled.');
-            _context.next = 32;
+            _context2.next = 32;
             break;
 
           case 29:
-            _context.prev = 29;
-            _context.t1 = _context['catch'](2);
-
-            console.log('Could not fetch jobs');
+            _context2.prev = 29;
+            _context2.t1 = _context2['catch'](2);
+            throw _context2.t1;
 
           case 32:
           case 'end':
-            return _context.stop();
+            return _context2.stop();
         }
       }
-    }, _callee, undefined, [[2, 29], [10, 14, 18, 26], [19,, 21, 25]]);
+    }, _callee2, undefined, [[2, 29], [10, 14, 18, 26], [19,, 21, 25]]);
   }));
 
-  return function recreateSchedule() {
-    return _ref.apply(this, arguments);
+  return function recreateScheduleForAllJobs() {
+    return _ref2.apply(this, arguments);
   };
 }();
+
+/**
+ * (Re)creates the schedule (crons) of a _JobSchedule
+ * @param {_JobSchedule} job The _JobSchedule
+ */
+var recreateJobSchedule = function recreateJobSchedule(job) {
+  destroyJob(job.id);
+  cronJobs[job.id] = createCronJobs(job);
+};
 
 /**
  * Stop all jobs and remove them from the list of jobs
@@ -144,30 +256,8 @@ var destroyJobs = function destroyJobs() {
   try {
     for (var _iterator2 = Object.keys(cronJobs)[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
       var key = _step2.value;
-      var _iteratorNormalCompletion3 = true;
-      var _didIteratorError3 = false;
-      var _iteratorError3 = undefined;
 
-      try {
-        for (var _iterator3 = cronJobs[key][Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-          var job = _step3.value;
-
-          job.stop();
-        }
-      } catch (err) {
-        _didIteratorError3 = true;
-        _iteratorError3 = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion3 && _iterator3.return) {
-            _iterator3.return();
-          }
-        } finally {
-          if (_didIteratorError3) {
-            throw _iteratorError3;
-          }
-        }
-      }
+      destroyJob(key);
     }
   } catch (err) {
     _didIteratorError2 = true;
@@ -185,6 +275,41 @@ var destroyJobs = function destroyJobs() {
   }
 
   cronJobs = {};
+};
+
+/**
+ * Destroy a planned cron job
+ * @param {String} id The _JobSchedule id
+ */
+var destroyJob = function destroyJob(id) {
+  var jobs = cronJobs[id];
+  if (jobs) {
+    var _iteratorNormalCompletion3 = true;
+    var _didIteratorError3 = false;
+    var _iteratorError3 = undefined;
+
+    try {
+      for (var _iterator3 = jobs[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+        var job = _step3.value;
+
+        job.stop();
+      }
+    } catch (err) {
+      _didIteratorError3 = true;
+      _iteratorError3 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion3 && _iterator3.return) {
+          _iterator3.return();
+        }
+      } finally {
+        if (_didIteratorError3) {
+          throw _iteratorError3;
+        }
+      }
+    }
+  }
+  delete cronJobs[id];
 };
 
 var createCronJobs = function createCronJobs(job) {
@@ -284,11 +409,11 @@ var daysOfWeekToCronString = function daysOfWeekToCronString(daysOfWeek) {
  * @param {Object=} params The parameters to pass to the request
  */
 var performJob = function () {
-  var _ref2 = _asyncToGenerator(regeneratorRuntime.mark(function _callee2(jobName, params) {
+  var _ref3 = _asyncToGenerator(regeneratorRuntime.mark(function _callee3(jobName, params) {
     var request;
-    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+    return regeneratorRuntime.wrap(function _callee3$(_context3) {
       while (1) {
-        switch (_context2.prev = _context2.next) {
+        switch (_context3.prev = _context3.next) {
           case 0:
             try {
               request = (0, _requestPromise2.default)({
@@ -311,14 +436,14 @@ var performJob = function () {
 
           case 1:
           case 'end':
-            return _context2.stop();
+            return _context3.stop();
         }
       }
-    }, _callee2, undefined);
+    }, _callee3, undefined);
   }));
 
-  return function performJob(_x, _x2) {
-    return _ref2.apply(this, arguments);
+  return function performJob(_x2, _x3) {
+    return _ref3.apply(this, arguments);
   };
 }();
 
